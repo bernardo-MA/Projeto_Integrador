7108ee0dc94b7421921bdfe5b4b7a4d28dbbb35e
BERNARDO MONTEIRO ALVES <06094214099@senacrs.edu.br>
2025-07-03 19:42:38 -0300

    Adicionado terminal.py

--- terminal.py
+++ terminal.py
@@ -1,1 +1,24 @@
 bebidas=[]
+pedido = []
+esc = 0
+def cardapio(op,esc,pedido):
+    op = ["Expresso","Cappuccino", "Latte"]
+    t = 1
+    while t < 2:
+        x = 1
+        for item in op:
+            print(x,"-", item)
+            x = x+1
+        esc = int(input("Qual deseja? "))
+        pedido += [op[esc-1]]
+        print(pedido[-1], "adicionado")
+        print("--------------------")
+        print("Pedido atual:")
+        for item in pedido:
+            print(item)
+        print("--------------------")
+        print("Deseja:")
+        print("1 - Adicionar ao pedido")
+        print("2 - Finalizar")
+        t = int(input(""))
+cardapio(bebidas,esc,pedido)
