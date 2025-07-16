12e3d9055102861ab4f660548a7ede7f1666e696
Samuel <vivi4n.sam@gmail.com>
2025-07-15 21:40:43 -0300

    adição do F11 funcionando

--- src/front.py
+++ src/front.py
@@ -1,4 +1,23 @@
 from tkinter import *
 janela = Tk()
 janela.title("STEAMCOFFEE")
+
+escfull=False
+
+def telacheia(event=None):
+    global escfull
+    escfull = not escfull
+    janela.attributes("-fullscreen", escfull)
+    
+def window(event=None):
+    global escfull
+    escfull = False
+    janela.attributes("-fullscreen", False)
+
+janela.bind("<F11>", telacheia)
+
+janela.bind("<Escape>", window)
+
+janela.geometry("1080x720")
+
 janela.mainloop()
