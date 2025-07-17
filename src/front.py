3170be6e4ef4fdac5a3065d56ff0ac12fe6b0eb1
Samuel <vivi4n.sam@gmail.com>
2025-07-16 21:11:21 -0300

    primeira label do front

--- src/front.py
+++ src/front.py
@@ -8,16 +8,18 @@ def telacheia(event=None):
     global escfull
     escfull = not escfull
     janela.attributes("-fullscreen", escfull)
-    
+
 def window(event=None):
     global escfull
     escfull = False
     janela.attributes("-fullscreen", False)
 
 janela.bind("<F11>", telacheia)
-
 janela.bind("<Escape>", window)
-
 janela.geometry("1080x720")
+
+label = Label (janela, text= "Hello World!", font=("Arial Bold", 25), bg="black", fg="white")
+
+label.grid(row=0, column=0, padx=440, pady=320)
 
 janela.mainloop()
