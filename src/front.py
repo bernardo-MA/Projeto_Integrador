f83f4a156d1df9bfe6267275878cfc4ee7e2419c
Samuel <vivi4n.sam@gmail.com>
2025-07-18 20:47:56 -0300

    alinhamento a esquerda

--- src/front.py
+++ src/front.py
@@ -5,6 +5,7 @@ class front:
         self.janela = Tk()
         self.escfull=None
         self.escfull=False
+
     def telacheia(self, event):
         self.escfull = not self.escfull
         self.janela.attributes("-fullscreen", self.escfull)
@@ -12,6 +13,13 @@ class front:
     def window(self, event):
         self.escfull = False
         self.janela.attributes("-fullscreen", False)
+
+    def telabarista(self):
+        print("Tela Barista")
+    
+
+
+
 
     def telainicial(self):
         self.janela.title("STEAMCOFFEE")
@@ -21,19 +29,19 @@ class front:
         self.janela.geometry("1280x720")
         self.janela.resizable(False,False)
 
-        label = Label (self.janela, text= "STEAMCOFFEE", font=("Arial Bold", 25), bg="black", fg="white")
-
-        label.grid(row=0, column=0, padx=0, pady=0)
+        label = Label (self.janela, text= "STEAMCOFFEE", font=("Arial Bold", 25),)        
+        texto1= Label (self.janela, text ="Selecione qual area deseja acessar", font=("Arial Bold", 20))
+        # label.grid( padx=2, pady=1)
+        label.grid(row=0, column=0, padx=2, pady=1, sticky="w")
+        texto1.grid(row=5, column=0)
 
         botao = Button (self.janela, text="Barista")
-        botao.grid(row=5, column=0, padx=0, pady=2)
+        botao.grid(row=7, column=0, padx=0, pady=2)
 
     def ativar(self):
         self.janela.mainloop()
 
 if __name__ == "__main__":
     apli=front()
-    # apli.telacheia()
-    # apli.window()
     apli.telainicial()
     apli.ativar()
