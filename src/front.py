f122abf56f2c35f8804a2bb8c38a824be0bfa912
Samuel <vivi4n.sam@gmail.com>
2025-07-18 21:58:49 -0300

    inicio das janelas

--- src/front.py
+++ src/front.py
@@ -5,6 +5,7 @@ class front:
         self.janela = Tk()
         self.escfull=None
         self.escfull=False
+        self.janelabarista = Tk()
 
     def telacheia(self, event):
         self.escfull = not self.escfull
@@ -15,11 +16,11 @@ class front:
         self.janela.attributes("-fullscreen", False)
 
     def telabarista(self):
-        print("Tela Barista")
+        self.janela.destroy()
+        self.janelabarista.mainloop()
     
-
-
-
+    def telagarcom(self):
+        print("Tela garçom")
 
     def telainicial(self):
         self.janela.title("STEAMCOFFEE")
@@ -28,15 +29,23 @@ class front:
         self.janela.bind("<Escape>", self.window)
         self.janela.geometry("1280x720")
         self.janela.resizable(False,False)
-
+        ## TEXTOS
         label = Label (self.janela, text= "STEAMCOFFEE", font=("Arial Bold", 25),)        
         texto1= Label (self.janela, text ="Selecione qual area deseja acessar", font=("Arial Bold", 20))
-        # label.grid( padx=2, pady=1)
-        label.grid(row=0, column=0, padx=2, pady=1, sticky="w")
-        texto1.grid(row=5, column=0)
+        label.grid(row=0, column=0, padx=0, pady=20, sticky="w")
+        espaco= Label(self.janela, text="")
+        espaco.grid(pady=20)
+        texto1.grid(row=28, column=0, pady=10, sticky="w")
+        ## BOTOES
+        bar = Button (self.janela, text="Barista", command=self.telabarista)
+        bar.grid(row=30, column=0, padx=5, pady=15, sticky="nsew")
 
-        botao = Button (self.janela, text="Barista")
-        botao.grid(row=7, column=0, padx=0, pady=2)
+        gar = Button (self.janela, text="Garçom")
+        gar.pack
+        gar.grid(row=35, column=0, padx=5, pady=15, sticky="nsew")
+
+        notas=Button (self.janela, text="Notas")
+        notas.grid(row=40, column=0, padx=5, pady=15, sticky="nsew")
 
     def ativar(self):
         self.janela.mainloop()
