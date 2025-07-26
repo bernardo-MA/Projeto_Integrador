d2e2934541a51a2bd10e3078f7805c88b7edc6f4
Bernardo Monteiro <monteirobernardo6@gmail.com>
2025-07-25 21:37:14 -0300

    Quase indo

--- src/garçomimagem.py
+++ src/garçomimagem.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 class telagarcom:
     def __init__(self):
-        self.lista= ["cafe latte"]
+        self.lista= []
         self.produto=''
     def Janela(self):
         # Configurações da janela
@@ -23,82 +23,107 @@ class telagarcom:
         self.torta = PhotoImage(file='img/torta.png')
         self.pastel = PhotoImage(file='img/pastel.png')
 
-        #Escrita garçom
-        self.nmenu = Label(self.garcom,text="GARÇOM",font=('Arial', 45, 'bold'),fg="white",bg='#38312D',)
-        self.nmenu.place(x=15,y=50)
-        self.seta = Button(self.garcom, image=self.st,command=self.incompleto,bg='#38312D',bd=0, )
-        self.seta.place(x=0,y=5)
+        #Escritas
+        self.nmenu = Label(self.garcom,text="GARÇOM",font=('Arial', 24, 'bold'),fg="#D9D9D9",bg='#38312D',)
+        self.nmenu.place(x=30,y=50)
+        linha1= Frame(self.garcom, bg="#D9D9D9", height=1, width=320)        
+        linha1.place(x=15,y=100)
+        linha1= Frame(self.garcom, bg="#D9D9D9", height=650, width=1)        
+        linha1.place(x=850,y=50)
+        
 
     def botoes(self):
         #frame para botoes
         self.cardapio = Frame(self.garcom)
-        self.cardapio.place(x=20,y=150)
+        self.cardapio.place(x=20,y=120)
+
+        self.digitas = Frame(self.garcom)
+        self.digitas.place()
 
         # Butoes do cardapio
-        self.opção1 = Button(self.cardapio, image=self.expresso, command=self.Expresso,bg='#38312D',bd=0)
+        self.opção1 = Button(self.cardapio, image=self.expresso, command=self.Expresso,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção1.pack(side='top')
 
-        self.opção2 = Button(self.cardapio, image=self.cappuccino, command=self.Cappuccino,bg='#38312D',bd=0)
+        self.opção2 = Button(self.cardapio, image=self.cappuccino, command=self.Cappuccino,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção2.pack(side='top')
 
-        self.opção3 = Button(self.cardapio, image=self.latte, command=self.Latte,bg='#38312D',bd=0)
+        self.opção3 = Button(self.cardapio, image=self.latte, command=self.Latte,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção3.pack(side='top')
 
-        self.opção4 = Button(self.cardapio, image=self.mocca, command=self.Mocca,bg='#38312D',bd=0)
+        self.opção4 = Button(self.cardapio, image=self.mocca, command=self.Mocca,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção4.pack(side='top')
 
-        self.opção5 = Button(self.cardapio, image=self.torta, command=self.Torta,bg='#38312D',bd=0)
+        self.opção5 = Button(self.cardapio, image=self.torta, command=self.Torta,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção5.pack(side='top')
 
-        self.opção6 = Button(self.cardapio, image=self.pdq, command=self.Pdq,bg='#38312D',bd=0)
+        self.opção6 = Button(self.cardapio, image=self.pdq, command=self.Pdq,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção6.pack(side='top')
 
-        self.opção7 = Button(self.cardapio, image=self.pastel, command=self.Pastel,bg='#38312D',bd=0)
+        self.opção7 = Button(self.cardapio, image=self.pastel, command=self.Pastel,bg='#38312D',bd=0,activebackground='#38312D')
         self.opção7.pack(side='top')
 
-        self.qtnt = Label(self.garcom, text="Quantidade:",font=('Arial',15, 'bold'),fg="white",bg='#38312D')
-        self.qtnt.place(x=280,y=680)
+        self.qtnt = Label(self.cardapio, text="Quantidade:",font=('Arial',15, 'bold'),fg="white",bg='#38312D')
+        self.qtnt.pack(side='bottom')
 
-        self.qtn = Entry(self.garcom, font=45)
-        self.qtn.place(x=425,y=681)
+        self.qtn = Entry(self.cardapio, font=45)
+        self.qtn.pack(side='bottom')
 
-        self.add = Button(self.garcom, text="Add",relief=RAISED,bd=2,command=self.quantidade)
-        self.add.place(x=620,y=680)
+        self.add = Button(self.cardapio, text="Add",relief=RAISED,bd=2,command=self.quantidade)
+        self.add.pack(side='bottom')
 
+        self.seta = Button(self.garcom, image=self.st,command=self.incompleto,bg='#38312D',bd=0, )
+        self.seta.place(x=0,y=5)
     def incompleto(self):
         print("ainda n funcional")
     #Funções para Botoes
+    def Outro(self):
+        self.opção1.config(bg="#38312D");self.opção1.config(state=NORMAL)
+        self.opção2.config(bg="#38312D");self.opção2.config(state=NORMAL)
+        self.opção3.config(bg="#38312D");self.opção3.config(state=NORMAL)
+        self.opção4.config(bg="#38312D");self.opção4.config(state=NORMAL)
+        self.opção5.config(bg="#38312D");self.opção5.config(state=NORMAL)
+        self.opção6.config(bg="#38312D");self.opção6.config(state=NORMAL)
+        self.opção7.config(bg="#38312D");self.opção7.config(state=NORMAL)
+
     def Expresso(self):
-        self.lista.append("Cafe expresso")
-        print(self.lista)
-        self.opção1.config(bg='green')
+        self.produto = "Expresso"
+        self.Outro()
         self.opção1.config(state=DISABLED)
     def Cappuccino(self):
         self.produto = "Cappuccino"
+        self.Outro()
+        self.opção2.config(state=DISABLED)
     def Latte(self):
         self.produto = "Latte"
+        self.Outro()
+        self.opção3.config(state=DISABLED)
     def Mocca(self):
         self.produto = 'Mocca'
+        self.Outro()
+        self.opção4.config(state=DISABLED)
     def Torta(self):
         self.produto = "Torta"
-        print(self.lista)
-        self.opção2.config(bg='green')
-        self.opção2.config(state=DISABLED)
+        self.Outro()
+        self.opção5.config(state=DISABLED)
     def Pdq(self):
         self.produto = 'Pao de Queijo'
+        self.Outro()
+        self.opção6.config(state=DISABLED)
     def Pastel(self):
         self.produto = "Pastel"
+        self.Outro()
+        self.opção7.config(state=DISABLED)
 
     def quantidade(self):
-        self.qtns = Entry.get()
+        self.qtns = self.qtn.get()
         self.lista.append (f'{self.qtns}x {self.produto}')
+        print(self.lista)
     def show(self):
         self.garcom.mainloop()
 
 
 if __name__ =="__main__":
     app=telagarcom()
-    # app.Imagens()
     app.Janela()
     app.botoes()
     app.show()
