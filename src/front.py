7c578847e1343d42dc0bf2b4eb1630d26b3496c2
Samuel <vivi4n.sam@gmail.com>
2025-07-28 23:45:30 -0300

    inserção da janela do garçom, conversa com o db 100% completa

--- src/front.py
+++ src/front.py
@@ -14,6 +14,28 @@ class front:
         self.cur=None
         self.pedido=[]
         self.datas = ["2025-07-28"]
+        self.lista=[]
+
+        ##IMAGENS
+        self.expresso = PhotoImage(file='img/expresso.png')
+        self.cappuccino = PhotoImage(file='img/cappuccino.png')
+        self.latte = PhotoImage(file='img/latte.png')
+        self.mocca = PhotoImage(file='img/mocca.png')
+        self.pdq = PhotoImage(file='img/paodequeijo.png')
+        self.torta = PhotoImage(file='img/torta.png')
+        self.pastel = PhotoImage(file='img/pastel.png')
+        self.concluido = PhotoImage(file='img/botaoconcluido.png')
+        self.confirmar_o = Image.open("img/botaoconfirmar.png")
+        self.confirmar_n = self.confirmar_o.resize((65,65))
+        self.confirmar = ImageTk.PhotoImage(self.confirmar_n)
+        self.retorno_o = Image.open("img/f5.png")
+        self.retorno_n = self.retorno_o.resize((125,125))
+        self.retorno = ImageTk.PhotoImage(self.retorno_n)
+        self.lixeira_o = Image.open("img/lixeira.png")
+        self.lixeira_n = self.lixeira_o.resize((65,65))
+        self.lixeira = ImageTk.PhotoImage(self.lixeira_n)
+        self.voltf5=PhotoImage(file="img/f5.png")
+
 
     def mysqlconnect(self):
         try:
@@ -30,6 +52,8 @@ class front:
         except pymysql.MySQLError as e:
             print(f"Deu erro porra {e}")
 
+
+
     def telacheia(self, event):
         self.escfull = not self.escfull
         self.janela.attributes("-fullscreen", self.escfull)
@@ -39,11 +63,134 @@ class front:
         self.janela.attributes("-fullscreen", False)
 
     ##FUNÇOES BOTOES
-    def confirmardata(self):
+    def confirmardata(self):    
         self.datan = self.dtnova.get()
         self.datas.append(self.datan)
         self.atend.destroy()
         self.telaatend()
+
+
+    def incompleto(self):
+        self.listaorg=", ".join(self.lista)
+        self.cur.execute('INSERT INTO barista (pedidocompl) VALUES (%s)',(self.listaorg,))
+        self.lista.clear()
+        self.garcom.destroy()
+        self.telagarcom()
+
+    def Outro(self):
+        self.opção1.config(bg="#38312D");self.opção1.config(state=NORMAL)
+        self.opção2.config(bg="#38312D");self.opção2.config(state=NORMAL)
+        self.opção3.config(bg="#38312D");self.opção3.config(state=NORMAL)
+        self.opção4.config(bg="#38312D");self.opção4.config(state=NORMAL)
+        self.opção5.config(bg="#38312D");self.opção5.config(state=NORMAL)
+        self.opção6.config(bg="#38312D");self.opção6.config(state=NORMAL)
+        self.opção7.config(bg="#38312D");self.opção7.config(state=NORMAL)
+
+    def Expresso(self):
+        self.produto = "Expresso"
+        self.Outro()
+        self.opção1.config(state=DISABLED)
+
+    def Cappuccino(self):
+        self.produto = "Cappuccino"
+        self.Outro()
+        self.opção2.config(state=DISABLED)
+    def Latte(self):
+        self.produto = "Latte"
+        self.Outro()
+        self.opção3.config(state=DISABLED)
+    def Mocca(self):
+        self.produto = 'Mocca'
+        self.Outro()
+        self.opção4.config(state=DISABLED)
+    def Torta(self):
+        self.produto = "Torta"
+        self.Outro()
+        self.opção5.config(state=DISABLED)
+    def Pdq(self):
+        self.produto = 'Pao de Queijo'
+        self.Outro()
+        self.opção6.config(state=DISABLED)
+    def Pastel(self):
+        self.produto = "Pastel"
+        self.Outro()
+        self.opção7.config(state=DISABLED)
+
+    def quantidade(self):
+        self.qtns = self.qtn.get()
+        self.feito = (f'{self.qtns}x {self.produto}')
+        self.lista.append (self.feito)
+        self.itens.insert(END,self.feito)
+        self.qtn.delete(0,END)
+        print(self.lista)
+
+
+
+    def botoes(self):
+        # frame para botoes
+        self.cardapio = Frame(self.garcom)
+        self.cardapio.place(x=20,y=120)
+
+        self.digitas = Frame(self.garcom,bg='#38312D',bd=0,)
+        self.digitas.place(x=860,y=425)
+        # Botoes do cardapio
+        self.opção1 = Button(self.cardapio, image=self.expresso, command=self.Expresso,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção1.pack(side='top')
+
+        self.opção2 = Button(self.cardapio, image=self.cappuccino, command=self.Cappuccino,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção2.pack(side='top')
+
+        self.opção3 = Button(self.cardapio, image=self.latte, command=self.Latte,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção3.pack(side='top')
+
+        self.opção4 = Button(self.cardapio, image=self.mocca, command=self.Mocca,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção4.pack(side='top')
+
+        self.opção5 = Button(self.cardapio, image=self.torta, command=self.Torta,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção5.pack(side='top')
+
+        self.opção6 = Button(self.cardapio, image=self.pdq, command=self.Pdq,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção6.pack(side='top')
+
+        self.opção7 = Button(self.cardapio, image=self.pastel, command=self.Pastel,bg='#38312D',bd=0,activebackground='#38312D')
+        self.opção7.pack(side='top')
+
+        #Botoes de quantidade 
+        self.qtnt = Label(self.digitas, text="Quantidade:",font=('Inknut Antiqua',15, 'bold'),fg="white",bg='#38312D')
+        self.qtnt.pack(side='top')
+
+        self.qtn = Entry(self.digitas, font=100, width=10)
+        self.qtn.pack(side='left')
+
+        self.add = Button(self.digitas,image=self.confirmar,command=self.quantidade,bg='#38312D',bd=0,)
+        self.add.pack(side='left')
+        #Botoes para delete 
+        self.returne = Button(self.garcom,image=self.voltf5,bg='#38312D',bd=0,)
+        self.returne.place(x=1100,y=490)
+        self.delete = customtkinter.CTkButton(
+            self.garcom,
+            text="X",
+            text_color="red",
+            font=("Arial", 40, "bold"),
+            fg_color="#D9D9D9",
+            corner_radius=15,
+            width=57,
+            height=57,
+        )
+        self.delete.place(x=1025, y=485)
+        # self.delete.place(x=950,y=425)
+
+        #Lista do q estas a adicionar
+        self.itens = Listbox(self.garcom,bg='#38312D',fg="#D9D9D9",font=("Inknut Antiqua", 12), width=30, height=6)
+        self.itens.place(x=875, y=75)
+
+        #seta
+        self.seta = Button(self.garcom, image=self.st,command=self.incompleto,bg='#38312D',bd=0,)
+        self.seta.place(x=0,y=5)
+
+        #Concluir
+        self.Concluido = Button(self.garcom,image=self.concluido,command=self.incompleto,bg='#38312D',bd=0,)
+        self.Concluido.place(x=900,y=575)
 
 
 
@@ -84,7 +231,7 @@ class front:
     def f5(self):
         self.atend.withdraw()
         self.janelabarista.withdraw()
-        self.janelagarcom.withdraw()
+        self.garcom.withdraw()
         self.janela.deiconify()
 
     def confirmbar(self):
@@ -106,7 +253,7 @@ class front:
         except:
             pass
         try:
-            self.janelagarcom.withdraw()
+            self.garcom.withdraw()
         except:
             pass
         try:
@@ -116,10 +263,31 @@ class front:
         self.janela.deiconify()
 
 
-        
+
     ## OUTRAS TELAS
     def telagarcom(self):
-        self.janelagarcom = Toplevel()
+        self.garcom = Toplevel(self.janela)
+        self.garcom.geometry('1280x720')
+        self.garcom.title("Garçom")
+
+        self.garcom.config(background='#38312D')
+
+
+        self.botoes()
+
+
+        self.back=Button(self.garcom, image=self.st, command=self.voltar, bg="#38312D", borderwidth=0)
+        self.back.place(x=0, y=0)
+
+        self.nmenu = Label(self.garcom,text="GARÇOM",font=('Inknut Antiqua', 40, 'bold'),fg="#D9D9D9",bg='#38312D',)
+        self.nmenu.place(x=50,y=-35)
+        self.Itens = Label(self.garcom, text="Itens selecionados", font=('Inknut Antiqua', 12, 'bold'),fg="#D9D9D9",bg='#38312D',)
+        self.Itens.place(x=875, y=40)
+        linha1= Frame(self.garcom, bg="#D9D9D9", height=1, width=360)        
+        linha1.place(x=15,y=100)
+        linha2= Frame(self.garcom, bg="#D9D9D9", height=650, width=1)        
+        linha2.place(x=850,y=50)
+
         self.janela.withdraw()
 
 
@@ -172,7 +340,7 @@ class front:
 
         linhab= Frame(self.janelabarista, bg="#D9D9D9", height=1, width=500)        
         linhab.grid(row=3, column=0, pady=3)
-        
+
 
     def telaatend(self):
         self.atend=Toplevel(self.janela)
@@ -188,7 +356,7 @@ class front:
         linha.pack(fill=X, padx=100, pady=(0, 30))
 
         seta=Button(self.atend, image=self.st,borderwidth=0,bg="#38312D", command=self.voltar)
-        seta.pack()
+        seta.place(x=1,y=1)
 
         frame_datas = Frame(self.atend, bg="#38312D")
         frame_datas.pack()
@@ -231,7 +399,7 @@ class front:
                                             )
             botao.grid(row=i//3, column=i%3, padx=10, pady=10)
 
-            
+
 
     ##TELA INCIAL
     def telainicial(self):
@@ -239,6 +407,8 @@ class front:
         self.janela.title("STEAMCOFFEE")
         self.janela.bind("<F11>", self.telacheia)
         self.janela.bind("<Escape>", self.window)
+        self.icon = PhotoImage(file='img/engrenagem.png')
+        self.janela.iconphoto(True, self.icon)
         self.janela.geometry("1280x720")
         # self.janela.resizable(False,False)
 
